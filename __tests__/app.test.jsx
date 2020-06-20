f3bdbcb93fa09004e2bbc9f2d090adeb7e3805c4
Victor <bearbur@gmail.com>
2020-06-20 22:55:18 +0300

    init electron app

---
+++ __tests__/app.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import App from '../src/App';
+
+describe('<App />', () => {
+  const wrap = mount(<App />);
+
+  it('renders', () => {
+    expect(wrap.find(App).exists()).toBe(true);
+  });
+});
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <span>games showcase</span>;
+};
+
+export default App;
